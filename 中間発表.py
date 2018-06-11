878eaf717231394cf381fe91a21f540b5267a616
前坂　怜 <pika_xy_01@i.softbank.jp>
2018-06-11 15:51:34 +0900

    else 消した

--- 中間発表.py
+++ 中間発表.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8; -*-
 
+import subprocess
 import RPi.GPIO as GPIO
 import time
 PIN=21
@@ -12,6 +13,10 @@ GPIO.setup(PIN,GPIO.IN)
 while True:
     pirValue = GPIO.input(PIN)
     if( pirValue == True):
-        print "PIR = ON"
-
+        subprocess.call('tvservice -o')
+        time.sleep(10)
+        subprocess.call('tvservice -p')
+        subprocess.call('fbset -depth 8')
+        subprocess.call('fbset -depth 32')
+        subprocess.call('xrefresh -d :0.0')
     time.sleep(0.1)
