f33e568116bfd75ad02e84a42a4c8776d2c4cc3b
前坂　怜 <pika_xy_01@i.softbank.jp>
2018-06-11 13:39:01 +0900

    開始テスト

---
+++ 中間発表.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8; -*-
+
+import RPi.GPIO as GPIO
+import time
+PIN=21
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(PIN,GPIO.IN)
+
+while True:
+pirValue = GPIO.input(PIN)
+if( pirValue == True):
+print "PIR = ON"
+else:
+print "PIR = OFF"
+
+time.sleep(0.1)
